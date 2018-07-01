536ae404a7672a4b23510ea7861317d34a52f3bd
Mykola <chmutkola@gmail.com>
2018-07-01 20:23:55 +0300

    add config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const config = require('config');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.end('hello man');
+router.use('/', function(req, res, next) {
+  res.end(config.get('Customer.dbConfig.url'));
 });
 
 module.exports = router;
